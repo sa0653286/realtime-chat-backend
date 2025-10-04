4ed750bf0a42967141b13965ae1e5be22aa853b6
Jaratto Riamusu <141940079+sa0653286@users.noreply.github.com>
2025-10-04 17:49:17 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI: "mongodb+srv://sa0653286:<db_password>@sa0653286.z7tyl9v.mongodb.net/?retryWrites=true&w=majority&appName=sa0653286",
+  JWT_SECRET: "sa0653286_secret_key"
+};
